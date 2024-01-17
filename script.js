f789c13593a30a2797c43179280f43bdb41021ba
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-01-17 23:35:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
- return l;
+ let array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                      'u', 'v', 'w', 'x', 'y', 'z'];
+	let str = "";
+	while(n--){
+		let value = parseInt(Math.random(array) * 36);
+		str += array[value];
+	}
 }
 
 // Do not change the code below.
